ffef6cd7fae0924daf1f62e58963855985b0a014
Pulsar-V <2671440661@qq.com>
2018-05-14 14:53:56 +0800

    fix rc json cpp

---
+++ src/cmake/rcRTMP.cmake
@@ -0,0 +1,10 @@
+set(LIB_NAME rcjson)
+set(SOURCE_FILES
+        src/rc_json/json_reader.cpp
+        src/rc_json/json_tool.h
+        src/rc_json/json_value.cpp
+        src/rc_json/json_valueiterator.inl
+        src/rc_json/json_writer.cpp
+        )
+include_directories(src/include)
+add_library(${LIB_NAME} SHARED ${SOURCE_FILES})
